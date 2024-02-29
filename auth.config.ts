d6e453ae7b3f3fcf20f500e1954e7d658f9efd44
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2024-02-29 12:32:42 +0000

    allow routes outside of dashboard

--- auth.config.ts
+++ auth.config.ts
@@ -1,20 +1,32 @@
 import type { NextAuthConfig } from 'next-auth';
 
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  DASHBOARD: '/dashboard',
+} as const;
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+      const isAuthenticated = Boolean(auth?.user);
+      const isLoginPage = nextUrl.pathname.includes(ROUTES.LOGIN);
+
+      // Grant access to the login page for unauthenticated users, ensuring they can log in.
+      if (isLoginPage && !isAuthenticated) {
+        return true;
       }
-      return true;
+
+      // Redirect authenticated users from the login page to the dashboard
+      if (isLoginPage && isAuthenticated) {
+        return Response.redirect(new URL(ROUTES.DASHBOARD, nextUrl));
+      }
+
+      // Determine access to dashboard routes based on authentication
+      return isAuthenticated;
     },
   },
   providers: [],
